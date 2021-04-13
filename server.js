b13b7cddd0a31ecce11b2aa6ccdf69dfd0bf9073
Peter Lebedev <LPeter@mail.ru>
2021-04-13 15:40:07 +0300

    first commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // where your node app starts
 
 // init project
+require('dotenv').config();
 var express = require('express');
 var app = express();
 
@@ -24,9 +25,38 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/timestamp/:date?", function(req, res) {
+  let reqDate = req.params.date;
+  let result = {};
+  if(reqDate) {
+    if(!isNaN(+reqDate)) {
+      // time in ms
+      let d = new Date(+reqDate);
+      result = {unix: d.getTime(), utc: d.toString()};
+    } else {
+      // try parse YYYY-MM-DD
+      let m = reqDate.match(/(\d{4})-(\d{2})-(\d{2})/);
+      if(!m) {
+        result = {error : "Invalid Date"};
+      } else {
+        let d = Date.parse(reqDate); // ms
+        if(d == 'Invalid Date') {
+          result = {error : "Invalid Date"};
+        } else {
+          d = new Date(d);
+          result = {unix: d.getTime(), utc: d.toString()};
+        }
+      }
+    }
+  } else {
+    // :date не указан - вернуть текущее время
+    result = {unix: Date.now(), utc: Date().toString()};
+  }
+  res.send(result);
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
